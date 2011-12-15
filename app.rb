8fb11e57358118f94ca7182c72715ff2bc13ece3
Caley Woods <caley.w@gmail.com>
2011-12-15 10:34:30 -0600

    added some meat to app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
 
+grove_api_key = ENV['grove_api_key']
+
 get '/' do
   "Hello World!"
 end
 
 post '/incoming' do
-  # do your magic
+  # Grove supported params are:
+  # service  - Name of the service (displayed in the chat room)
+  # message  - The message sent to the chat room (or notification)
+  # url      - (Optional) Service URL for the web client
+  # icon_url - (Optional) Icon for the service. Similar to a gravatar for a user.
+  from    = params[:From]
+  body    = params[:Body]
+  message = "SMS reply from: #{from} - #{body}"
+  service = "Hubot"
+  RestClient.post "https://grove.io/api/notice/#{grove_api_key}/", :service => service, :message => message
 end
